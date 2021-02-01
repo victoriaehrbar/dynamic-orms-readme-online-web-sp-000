ab722d21755e10b7f57d00f0a575cf960fddfd00
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-01 23:24:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,10 +53,9 @@ class Song
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
-  def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-    DB[:conn].execute(sql)
-  end
+ def self.find_by_name(name)
+  DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
+end
 
 end
 
