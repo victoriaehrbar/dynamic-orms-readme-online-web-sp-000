bd2f39b19a103f16de0f7404d399c34009b5e0c9
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-01 23:28:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,35 +21,21 @@ class Song
     column_names.compact
   end
 
-  values = []
- 
-self.class.column_names.each do |col_name|
-  values << "'#{send(col_name)}'" unless send(col_name).nil?
-end
-
-def col_names_for_insert
-  self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-end
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
-  
-  def values_for_insert
-  values = []
-  self.class.column_names.each do |col_name|
-    values << "'#{send(col_name)}'" unless send(col_name).nil?
-  end
-  values.join(", ")
-end
 
-def save
-  DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
- 
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-end
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
 
   def table_name_for_insert
     self.class.table_name
@@ -67,11 +53,9 @@ end
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
- def self.find_by_name(name)
-  DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
-end
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 
 end
-
-
-
