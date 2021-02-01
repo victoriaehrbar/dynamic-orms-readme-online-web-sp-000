11463d15ab21087110b2d6a162e7b60510358a6e
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-01 23:51:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,8 +52,8 @@ end
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
+  self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+end
 
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
